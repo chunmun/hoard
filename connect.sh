a1668394f822f55a8cec88980e9e11a1f336097e
Chunmun <xilasirc@gmail.com>
2025-08-15 07:16:15 +0800

    Fix incorrect oracle-db docker-compose.yml
    + Add dogs table by default

--- connect.sh
+++ connect.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -it --network=oracledb_default oracledb-client sqlplus system/12345@oracledb:1521/XEPDB1
+docker run --rm -i --network=oracledb_default oracledb-client sqlplus system/12345@oracledb:1521/XEPDB1
---
+++ database/setup_scripts/01_create_dogs_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Dogs (
+    breed VARCHAR2(50) PRIMARY KEY
+);
+
+INSERT INTO Dogs (breed) VALUES ('Golden Retriever');
+INSERT INTO Dogs (breed) VALUES ('Labrador Retriever');
+INSERT INTO Dogs (breed) VALUES ('German Shepherd');
+INSERT INTO Dogs (breed) VALUES ('French Bulldog');
+INSERT INTO Dogs (breed) VALUES ('Beagle');
+INSERT INTO Dogs (breed) VALUES ('Poodle');
+INSERT INTO Dogs (breed) VALUES ('Bulldog');
+INSERT INTO Dogs (breed) VALUES ('Rottweiler');
+INSERT INTO Dogs (breed) VALUES ('Siberian Husky');
+INSERT INTO Dogs (breed) VALUES ('Dachshund');
